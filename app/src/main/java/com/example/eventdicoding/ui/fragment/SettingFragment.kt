2cf352bcb163b2b77ef014f20498801554a1d93a
SanoMush <mustofahusni27@gmail.com>
2024-10-26 13:32:23 +0700

    DarkMode

--- app/src/main/java/com/example/eventdicoding/ui/fragment/SettingFragment.kt
+++ app/src/main/java/com/example/eventdicoding/ui/fragment/SettingFragment.kt
@@ -1,11 +1,15 @@
 package com.example.eventdicoding.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 import com.example.eventdicoding.R
+import com.google.android.material.switchmaterial.SwitchMaterial
+import android.util.Log
 
 class SettingFragment : Fragment() {
 
@@ -13,7 +17,21 @@ class SettingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        val view = inflater.inflate(R.layout.fragment_setting, container, false)
+
+        val switchTheme = view.findViewById<SwitchMaterial>(R.id.switchDarkMode)
+        if (switchTheme == null) {
+            Log.e("SettingFragment", "Switch not found in layout")
+        }
+
+        switchTheme?.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+        return view
     }
 }
